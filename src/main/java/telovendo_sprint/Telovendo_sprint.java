49c00c24b1e5f60cfd4c91721f4b4590300fe351
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-11-11 17:07:28 -0300

    primer commit

---
+++ src/main/java/telovendo_sprint/Telovendo_sprint.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package telovendo_sprint;
+
+/**
+ *
+ * @author Kathy
+ */
+public class Telovendo_sprint {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
